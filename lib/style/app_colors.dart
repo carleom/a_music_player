b972991b4ecff2eacba6133ba4a023609d58779e
carleom <carleomitolo@gmail.com>
2023-10-19 15:48:47 -0400

    finally found an updated rss package for flutter 3

--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
 
 const availableColors = <Color>[
-  Color(0xFF9ACD32), // Yellow Green
-  Color(0xFF00FA9A), // Medium Spring Green
-  Color(0xFFF08080), // Light Coral
-  Color(0xFF6495ED), // Cornflower Blue
-  Color(0xFFC8B6FF), // Lavender Blue
-  Color(0xFFE2C09F), // Pale Peach
-  Color(0xFFAABBCC), // Light Steel Blue
-  Color(0xFFFFC0CB), // Pink
-  Color(0xFFFFD700), // Gold
-  Color(0xFF008080), // Teal
-  Color(0xFF9932CC), // Dark Orchid
-  Color(0xFF00CED1), // Dark Turquoise
-  Color(0xFFE9967A), // Dark Salmon
-  Color(0xFFFA8072), // Salmon
-  Color(0xFFBDB76B), // Dark Khaki
-  Color(0xFF5D6CC2), // Slate Blue
-  Color(0xFFF8C733), // Cadmium Yellow
-  Color(0xFF2196F3), // Material Blue
-  Color(0xFFF44336), // Red
-  Color(0xFF4CAF50), // Green
-  Color(0xFFFF9800), // Orange
-  Color(0xFFE91E63), // Material Pink
-  Color(0xFF009688), // Material Teal
-  Color(0xFFCDDC39), // Lime
-  Color(0xFF00BCD4), // Material Cyan
-  Color(0xFFC4A092), // Material Brown
-  Color(0xFF9E9E9E), // Material Grey
+  Color(0xFF282828), // bg
+  Color(0xFFcc241d), // red
+  Color(0xFF98971a), // green
+  Color(0xFFd79921), // yellow
+  Color(0xFF458588), // blue
+  Color(0xFFb16286), // purple
+  Color(0xFF689d6a), // aqua
+  Color(0xFFa89984), // gray
+  Color(0xFF928374), // gray
+  Color(0xFFfb4934), // red-dim
+  Color(0xFFb8bb26), // green-dim
+  Color(0xFFfabd2f), // yellow-dim
+  Color(0xFF458588), // blue-dim
+  Color(0xFFd3869b), // purple-dim
+  Color(0xFF8ec07c), // green-dim
+  Color(0xFFebdbb2), // fg
+  Color(0xFF1d2021), // bg0_h
+  Color(0xFF282828), // bg0
+  Color(0xFF3c3836), // bg1
+  Color(0xFF504945), // bg2
+  Color(0xFF665c54), // bg3
+  Color(0xFF7c6f64), // bg4
+  Color(0xFF928374), // gray
+  Color(0xFFd65d0e), // orange
+  Color(0xFFfe8019), // orange dim
+  Color(0xFF32302f), // bg0_s
+  Color(0xFFa89984), // fg4
+  Color(0xFFbdae93), // fg3
+  Color(0xFFd5c4a1), // fg2
+  Color(0xFFebdbb2), // fg1
+  Color(0xFFfbf1c7), // fg0
 ];
